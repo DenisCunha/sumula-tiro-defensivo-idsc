bb9c3ad4edec50e6748eaae48742b6d71c24ca90
deniscunha <ded_2004@hotmail.com>
2024-07-20 11:12:02 -0300

    adjusting data base idsc.sql

--- install/idsc.sql
+++ install/idsc.sql
@@ -12,7 +12,8 @@ CREATE TABLE IF NOT EXISTS `atletas` (
   `id` int(11) NOT NULL,
   `nome` varchar(255) NOT NULL,
   `divisao_id` int(2) NOT NULL,
-  `user_id` int(2) NOT NULL DEFAULT 0
+  `user_id` int(2) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 -- --------------------------------------------------------
@@ -25,16 +26,21 @@ CREATE TABLE IF NOT EXISTS `competicao` (
   `evento_id` int(11) NOT NULL,
   `nomeevento` varchar(255) NOT NULL,
   `dataevento` varchar(10) NOT NULL,
+  `stage` int(2) NOT NULL DEFAULT 1,
+  `target` int(2) NOT NULL DEFAULT 1,
+  `target1` int(2) NOT NULL DEFAULT 1,
+  `shots` int(4) NOT NULL DEFAULT 1,
   `image` varchar(250) NOT NULL,
-  `dso_id` int(2) NOT NULL DEFAULT 0
+  `dso_id` int(2) NOT NULL DEFAULT 0,
+   PRIMARY KEY (`evento_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
 -- Extraindo dados da tabela `competicao`
 --
 
-INSERT INTO `competicao` (`evento_id`, `nomeevento`, `dataevento`, `image`, `dso_id`) VALUES
-(1, 'Treino IDSC - Teste', '28/04/2024', 'asset/img/logoclube.png', 1);
+INSERT INTO `competicao` (`evento_id`, `nomeevento`, `dataevento`, `stage`, `target`, `target1`, `shots`, `image`, `dso_id`) VALUES
+(1, 'Treino IDSC - Teste', '28/04/2024', 2, 10, 1, 21, 'asset/img/logoclube.png', 1);
 
 -- --------------------------------------------------------
 
@@ -44,7 +50,8 @@ INSERT INTO `competicao` (`evento_id`, `nomeevento`, `dataevento`, `image`, `dso
 
 CREATE TABLE IF NOT EXISTS `divisao` (
   `divisao_id` int(11) NOT NULL,
-  `nome` varchar(255) NOT NULL
+  `nome` varchar(255) NOT NULL,
+  PRIMARY KEY (`divisao_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
@@ -55,8 +62,30 @@ INSERT INTO `divisao` (`divisao_id`, `nome`) VALUES
 (1, 'HP - Hard Pistol'),
 (2, 'SP - Soft Pistol'),
 (3, 'CP - Custom Pistol'),
-(4, 'OP - Optic Pistol'),
-(5, 'HR - Hard Revolver');
+(4, 'CL - Classic Pistol'),
+(5, 'OP - Optic Pistol'),
+(6, 'MP - Mini Pistol'),
+(7, 'RP - Rimfire Pistol'),
+(8, 'LR - Large Revolver'),
+(9, 'HR - Hard Revolver'),
+(10, 'CR - Custom Revolver'),
+(11, 'MR - Mini Revolver'),
+(12, 'RR - Rimfire Revolver'),
+(13, 'PS - Pump Shotgun'),
+(14, 'SS - Semiauto Shotgun'),
+(15, 'CA - Carbine'),
+(16, 'OC - Optic Carbine'),
+(17, 'PC - Pistol Caliber Carbine'),
+(18, 'PCO - Pistol Caliber Optical Carbine'),
+(19, 'SC - Shotgun Caliber Carbine'),
+(20, 'RC - Rimfire Carbine'),
+(21, 'HE - Heavy Multigun'),
+(22, 'OM - optic Multigun'),
+(23, 'HM - Hard Multigun'),
+(24, 'HOM - Hard Optic Multigun'),
+(25, 'RM - Rimfire Multigun'),
+(26, 'AIR PISTOL (AP)'),
+(27, 'AIR CARBINE (AC)');
 
 -- --------------------------------------------------------
 
@@ -70,6 +99,11 @@ CREATE TABLE IF NOT EXISTS `pistas` (
   `divisao` int(2) NOT NULL,
   `competicao_id` int(2) NOT NULL,
   `zero` int(11) NOT NULL,
+  `stage` int(2) NOT NULL,
+  `dois` int(2) NOT NULL DEFAULT 0,
+  `cinco` int(2) NOT NULL DEFAULT 0,
+  `miss` int(2) NOT NULL DEFAULT 0,
+  `shots` int(4) NOT NULL DEFAULT 0,
   `t1` int(2) NOT NULL,
   `t2` int(2) NOT NULL,
   `t3` int(2) NOT NULL,
@@ -94,389 +128,8 @@ CREATE TABLE IF NOT EXISTS `pistas` (
   `fs` float(5,2) NOT NULL,
   `timer` float(5,2) NOT NULL,
   `dq` int(1) NOT NULL,
-  `total` float(5,2) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `resultados`
---
-
-CREATE TABLE IF NOT EXISTS `resultados` (
-  `id` int(11) NOT NULL,
-  `nome` varchar(250) NOT NULL,
-  `divisao` int(2) NOT NULL,
-  `competicao_id` int(2) NOT NULL,
-  `zero` int(11) NOT NULL,
-  `t1` int(2) NOT NULL,
-  `t2` int(2) NOT NULL,
-  `t3` int(2) NOT NULL,
-  `t4` int(2) NOT NULL,
-  `t5` int(2) NOT NULL,
-  `t6` int(2) NOT NULL,
-  `t7` int(2) NOT NULL,
-  `t8` int(2) NOT NULL,
-  `t9` int(2) NOT NULL,
-  `t10` int(2) NOT NULL,
-  `t11` int(2) NOT NULL,
-  `m1` int(11) NOT NULL,
-  `m2` int(11) NOT NULL,
-  `m3` int(11) NOT NULL,
-  `m4` int(11) NOT NULL,
-  `m5` int(11) NOT NULL,
-  `m6` int(11) NOT NULL,
-  `ep` int(11) NOT NULL,
-  `ae` int(11) NOT NULL,
-  `uc` int(11) NOT NULL,
-  `ns` int(2) NOT NULL,
-  `fs` float(5,2) NOT NULL,
-  `timer` float(5,2) NOT NULL,
-  `dq` int(1) NOT NULL,
-  `total` float(5,2) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `resultados2`
---
-
-CREATE TABLE IF NOT EXISTS `resultados2` (
-  `id` int(11) NOT NULL,
-  `nome` varchar(250) NOT NULL,
-  `divisao` varchar(55) NOT NULL,
-  `competicao_id` int(2) NOT NULL,
-  `zero` int(11) NOT NULL,
-  `t1` int(2) NOT NULL,
-  `t2` int(2) NOT NULL,
-  `t3` int(2) NOT NULL,
-  `t4` int(2) NOT NULL,
-  `t5` int(2) NOT NULL,
-  `t6` int(2) NOT NULL,
-  `t7` int(2) NOT NULL,
-  `t8` int(2) NOT NULL,
-  `t9` int(2) NOT NULL,
-  `t10` int(2) NOT NULL,
-  `t11` int(2) NOT NULL,
-  `m1` int(11) NOT NULL,
-  `m2` int(11) NOT NULL,
-  `m3` int(11) NOT NULL,
-  `m4` int(11) NOT NULL,
-  `m5` int(11) NOT NULL,
-  `m6` int(11) NOT NULL,
-  `ep` int(11) NOT NULL,
-  `ae` int(11) NOT NULL,
-  `uc` int(11) NOT NULL,
-  `ns` int(2) NOT NULL,
-  `fs` float(5,2) NOT NULL,
-  `timer` float(5,2) NOT NULL,
-  `dq` int(1) NOT NULL,
-  `total` float(5,2) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `resultados3`
---
-
-CREATE TABLE IF NOT EXISTS `resultados3` (
-  `id` int(11) NOT NULL,
-  `nome` varchar(250) NOT NULL,
-  `divisao` varchar(55) NOT NULL,
-  `competicao_id` int(2) NOT NULL,
-  `t1` int(2) NOT NULL,
-  `t2` int(2) NOT NULL,
-  `t3` int(2) NOT NULL,
-  `t4` int(2) NOT NULL,
-  `t5` int(2) NOT NULL,
-  `t6` int(2) NOT NULL,
-  `t7` int(2) NOT NULL,
-  `t8` int(2) NOT NULL,
-  `t9` int(2) NOT NULL,
-  `t10` int(2) NOT NULL,
-  `t11` int(2) NOT NULL,
-  `m1` int(11) NOT NULL,
-  `m2` int(11) NOT NULL,
-  `m3` int(11) NOT NULL,
-  `m4` int(11) NOT NULL,
-  `m5` int(11) NOT NULL,
-  `m6` int(11) NOT NULL,
-  `ep` int(11) NOT NULL,
-  `ae` int(11) NOT NULL,
-  `uc` int(11) NOT NULL,
-  `ns` int(2) NOT NULL,
-  `fs` float(5,2) NOT NULL,
-  `timer` float(5,2) NOT NULL,
-  `dq` int(1) NOT NULL,
-  `total` float(5,2) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `resultados4`
---
-
-CREATE TABLE IF NOT EXISTS `resultados4` (
-  `id` int(11) NOT NULL,
-  `nome` varchar(250) NOT NULL,
-  `divisao` varchar(55) NOT NULL,
-  `competicao_id` int(2) NOT NULL,
-  `t1` int(2) NOT NULL,
-  `t2` int(2) NOT NULL,
-  `t3` int(2) NOT NULL,
-  `t4` int(2) NOT NULL,
-  `t5` int(2) NOT NULL,
-  `t6` int(2) NOT NULL,
-  `t7` int(2) NOT NULL,
-  `t8` int(2) NOT NULL,
-  `t9` int(2) NOT NULL,
-  `t10` int(2) NOT NULL,
-  `t11` int(2) NOT NULL,
-  `m1` int(11) NOT NULL,
-  `m2` int(11) NOT NULL,
-  `m3` int(11) NOT NULL,
-  `m4` int(11) NOT NULL,
-  `m5` int(11) NOT NULL,
-  `m6` int(11) NOT NULL,
-  `ep` int(11) NOT NULL,
-  `ae` int(11) NOT NULL,
-  `uc` int(11) NOT NULL,
-  `ns` int(2) NOT NULL,
-  `fs` float(5,2) NOT NULL,
-  `timer` float(5,2) NOT NULL,
-  `dq` int(1) NOT NULL,
-  `total` float(5,2) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `resultados5`
---
-
-CREATE TABLE IF NOT EXISTS `resultados5` (
-  `id` int(11) NOT NULL,
-  `nome` varchar(250) NOT NULL,
-  `divisao` int(2) NOT NULL,
-  `competicao_id` int(2) NOT NULL,
-  `t1` int(2) NOT NULL,
-  `t2` int(2) NOT NULL,
-  `t3` int(2) NOT NULL,
-  `t4` int(2) NOT NULL,
-  `t5` int(2) NOT NULL,
-  `t6` int(2) NOT NULL,
-  `t7` int(2) NOT NULL,
-  `t8` int(2) NOT NULL,
-  `t9` int(2) NOT NULL,
-  `t10` int(2) NOT NULL,
-  `t11` int(2) NOT NULL,
-  `m1` int(11) NOT NULL,
-  `m2` int(11) NOT NULL,
-  `m3` int(11) NOT NULL,
-  `m4` int(11) NOT NULL,
-  `m5` int(11) NOT NULL,
-  `m6` int(11) NOT NULL,
-  `ep` int(11) NOT NULL,
-  `ae` int(11) NOT NULL,
-  `uc` int(11) NOT NULL,
-  `ns` int(2) NOT NULL,
-  `fs` float(5,2) NOT NULL,
-  `timer` float(5,2) NOT NULL,
-  `dq` int(1) NOT NULL,
-  `total` float(5,2) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `resultados6`
---
-
-CREATE TABLE IF NOT EXISTS `resultados6` (
-  `id` int(11) NOT NULL,
-  `nome` varchar(250) NOT NULL,
-  `divisao` int(2) NOT NULL,
-  `competicao_id` int(2) NOT NULL,
-  `t1` int(2) NOT NULL,
-  `t2` int(2) NOT NULL,
-  `t3` int(2) NOT NULL,
-  `t4` int(2) NOT NULL,
-  `t5` int(2) NOT NULL,
-  `t6` int(2) NOT NULL,
-  `t7` int(2) NOT NULL,
-  `t8` int(2) NOT NULL,
-  `t9` int(2) NOT NULL,
-  `t10` int(2) NOT NULL,
-  `t11` int(2) NOT NULL,
-  `m1` int(11) NOT NULL,
-  `m2` int(11) NOT NULL,
-  `m3` int(11) NOT NULL,
-  `m4` int(11) NOT NULL,
-  `m5` int(11) NOT NULL,
-  `m6` int(11) NOT NULL,
-  `ep` int(11) NOT NULL,
-  `ae` int(11) NOT NULL,
-  `uc` int(11) NOT NULL,
-  `ns` int(2) NOT NULL,
-  `fs` float(5,2) NOT NULL,
-  `timer` float(5,2) NOT NULL,
-  `dq` int(1) NOT NULL,
-  `total` float(5,2) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `resultados7`
---
-
-CREATE TABLE IF NOT EXISTS `resultados7` (
-  `id` int(11) NOT NULL,
-  `nome` varchar(250) NOT NULL,
-  `divisao` int(2) NOT NULL,
-  `competicao_id` int(2) NOT NULL,
-  `t1` int(2) NOT NULL,
-  `t2` int(2) NOT NULL,
-  `t3` int(2) NOT NULL,
-  `t4` int(2) NOT NULL,
-  `t5` int(2) NOT NULL,
-  `t6` int(2) NOT NULL,
-  `t7` int(2) NOT NULL,
-  `t8` int(2) NOT NULL,
-  `t9` int(2) NOT NULL,
-  `t10` int(2) NOT NULL,
-  `t11` int(2) NOT NULL,
-  `m1` int(11) NOT NULL,
-  `m2` int(11) NOT NULL,
-  `m3` int(11) NOT NULL,
-  `m4` int(11) NOT NULL,
-  `m5` int(11) NOT NULL,
-  `m6` int(11) NOT NULL,
-  `ep` int(11) NOT NULL,
-  `ae` int(11) NOT NULL,
-  `uc` int(11) NOT NULL,
-  `ns` int(2) NOT NULL,
-  `fs` float(5,2) NOT NULL,
-  `timer` float(5,2) NOT NULL,
-  `dq` int(1) NOT NULL,
-  `total` float(5,2) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `resultados8`
---
-
-CREATE TABLE IF NOT EXISTS `resultados8` (
-  `id` int(11) NOT NULL,
-  `nome` varchar(250) NOT NULL,
-  `divisao` int(2) NOT NULL,
-  `competicao_id` int(2) NOT NULL,
-  `t1` int(2) NOT NULL,
-  `t2` int(2) NOT NULL,
-  `t3` int(2) NOT NULL,
-  `t4` int(2) NOT NULL,
-  `t5` int(2) NOT NULL,
-  `t6` int(2) NOT NULL,
-  `t7` int(2) NOT NULL,
-  `t8` int(2) NOT NULL,
-  `t9` int(2) NOT NULL,
-  `t10` int(2) NOT NULL,
-  `t11` int(2) NOT NULL,
-  `m1` int(11) NOT NULL,
-  `m2` int(11) NOT NULL,
-  `m3` int(11) NOT NULL,
-  `m4` int(11) NOT NULL,
-  `m5` int(11) NOT NULL,
-  `m6` int(11) NOT NULL,
-  `ep` int(11) NOT NULL,
-  `ae` int(11) NOT NULL,
-  `uc` int(11) NOT NULL,
-  `ns` int(2) NOT NULL,
-  `fs` float(5,2) NOT NULL,
-  `timer` float(5,2) NOT NULL,
-  `dq` int(1) NOT NULL,
-  `total` float(5,2) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `resultados9`
---
-
-CREATE TABLE IF NOT EXISTS `resultados9` (
-  `id` int(11) NOT NULL,
-  `nome` varchar(250) NOT NULL,
-  `divisao` int(2) NOT NULL,
-  `competicao_id` int(2) NOT NULL,
-  `t1` int(2) NOT NULL,
-  `t2` int(2) NOT NULL,
-  `t3` int(2) NOT NULL,
-  `t4` int(2) NOT NULL,
-  `t5` int(2) NOT NULL,
-  `t6` int(2) NOT NULL,
-  `t7` int(2) NOT NULL,
-  `t8` int(2) NOT NULL,
-  `t9` int(2) NOT NULL,
-  `t10` int(2) NOT NULL,
-  `t11` int(2) NOT NULL,
-  `m1` int(11) NOT NULL,
-  `m2` int(11) NOT NULL,
-  `m3` int(11) NOT NULL,
-  `m4` int(11) NOT NULL,
-  `m5` int(11) NOT NULL,
-  `m6` int(11) NOT NULL,
-  `ep` int(11) NOT NULL,
-  `ae` int(11) NOT NULL,
-  `uc` int(11) NOT NULL,
-  `ns` int(2) NOT NULL,
-  `fs` float(5,2) NOT NULL,
-  `timer` float(5,2) NOT NULL,
-  `dq` int(1) NOT NULL,
-  `total` float(5,2) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `resultados10`
---
-
-CREATE TABLE IF NOT EXISTS `resultados10` (
-  `id` int(11) NOT NULL,
-  `nome` varchar(250) NOT NULL,
-  `divisao` int(2) NOT NULL,
-  `competicao_id` int(2) NOT NULL,
-  `t1` int(2) NOT NULL,
-  `t2` int(2) NOT NULL,
-  `t3` int(2) NOT NULL,
-  `t4` int(2) NOT NULL,
-  `t5` int(2) NOT NULL,
-  `t6` int(2) NOT NULL,
-  `t7` int(2) NOT NULL,
-  `t8` int(2) NOT NULL,
-  `t9` int(2) NOT NULL,
-  `t10` int(2) NOT NULL,
-  `t11` int(2) NOT NULL,
-  `m1` int(11) NOT NULL,
-  `m2` int(11) NOT NULL,
-  `m3` int(11) NOT NULL,
-  `m4` int(11) NOT NULL,
-  `m5` int(11) NOT NULL,
-  `m6` int(11) NOT NULL,
-  `ep` int(11) NOT NULL,
-  `ae` int(11) NOT NULL,
-  `uc` int(11) NOT NULL,
-  `ns` int(2) NOT NULL,
-  `fs` float(5,2) NOT NULL,
-  `timer` float(5,2) NOT NULL,
-  `dq` int(1) NOT NULL,
-  `total` float(5,2) NOT NULL
+  `total` float(5,2) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 -- --------------------------------------------------------
@@ -500,7 +153,8 @@ CREATE TABLE IF NOT EXISTS `total_prova` (
   `p8` float(5,2) NOT NULL DEFAULT 0.00,
   `p9` float(5,2) NOT NULL DEFAULT 0.00,
   `p10` float(5,2) NOT NULL DEFAULT 0.00,
-  `soma` float(5,2) NOT NULL DEFAULT 0.00
+  `soma` float(5,2) NOT NULL DEFAULT 0.00,
+  PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 -- --------------------------------------------------------
@@ -513,207 +167,7 @@ CREATE TABLE IF NOT EXISTS `usuarios` (
   `user_id` int(11) NOT NULL,
   `login` varchar(30) DEFAULT NULL,
   `senha` varchar(40) DEFAULT NULL,
-  `nivel` int(2) DEFAULT 0
+  `nivel` int(2) DEFAULT 0,
+  PRIMARY KEY (`user_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-
---
--- Índices para tabelas despejadas
---
-
---
--- Índices para tabela `atletas`
---
-ALTER TABLE `atletas`
-  ADD PRIMARY KEY (`id`);
-
---
--- Índices para tabela `competicao`
---
-ALTER TABLE `competicao`
-  ADD PRIMARY KEY (`evento_id`);
-
---
--- Índices para tabela `divisao`
---
-ALTER TABLE `divisao`
-  ADD PRIMARY KEY (`divisao_id`);
-
---
--- Índices para tabela `pistas`
---
-ALTER TABLE `pistas`
-  ADD PRIMARY KEY (`id`);
-
---
--- Índices para tabela `resultados`
---
-ALTER TABLE `resultados`
-  ADD PRIMARY KEY (`id`);
-
---
--- Índices para tabela `resultados2`
---
-ALTER TABLE `resultados2`
-  ADD PRIMARY KEY (`id`);
-
---
--- Índices para tabela `resultados3`
---
-ALTER TABLE `resultados3`
-  ADD PRIMARY KEY (`id`);
-
---
--- Índices para tabela `resultados4`
---
-ALTER TABLE `resultados4`
-  ADD PRIMARY KEY (`id`);
-
---
--- Índices para tabela `resultados5`
---
-ALTER TABLE `resultados5`
-  ADD PRIMARY KEY (`id`);
-
---
--- Índices para tabela `resultados6`
---
-ALTER TABLE `resultados6`
-  ADD PRIMARY KEY (`id`);
-
---
--- Índices para tabela `resultados7`
---
-ALTER TABLE `resultados7`
-  ADD PRIMARY KEY (`id`);
-
---
--- Índices para tabela `resultados8`
---
-ALTER TABLE `resultados8`
-  ADD PRIMARY KEY (`id`);
-
---
--- Índices para tabela `resultados9`
---
-ALTER TABLE `resultados9`
-  ADD PRIMARY KEY (`id`);
-
---
--- Índices para tabela `resultados10`
---
-ALTER TABLE `resultados10`
-  ADD PRIMARY KEY (`id`);
-
---
--- Índices para tabela `total_prova`
---
-ALTER TABLE `total_prova`
-  ADD PRIMARY KEY (`id`);
-
---
--- Índices para tabela `usuarios`
---
-ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`user_id`);
-
---
--- AUTO_INCREMENT de tabelas despejadas
---
-
---
--- AUTO_INCREMENT de tabela `atletas`
---
-ALTER TABLE `atletas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de tabela `competicao`
---
-ALTER TABLE `competicao`
-  MODIFY `evento_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT de tabela `divisao`
---
-ALTER TABLE `divisao`
-  MODIFY `divisao_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
--- AUTO_INCREMENT de tabela `pistas`
---
-ALTER TABLE `pistas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de tabela `resultados`
---
-ALTER TABLE `resultados`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de tabela `resultados2`
---
-ALTER TABLE `resultados2`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de tabela `resultados3`
---
-ALTER TABLE `resultados3`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de tabela `resultados4`
---
-ALTER TABLE `resultados4`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de tabela `resultados5`
---
-ALTER TABLE `resultados5`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de tabela `resultados6`
---
-ALTER TABLE `resultados6`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de tabela `resultados7`
---
-ALTER TABLE `resultados7`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de tabela `resultados8`
---
-ALTER TABLE `resultados8`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de tabela `resultados9`
---
-ALTER TABLE `resultados9`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de tabela `resultados10`
---
-ALTER TABLE `resultados10`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de tabela `total_prova`
---
-ALTER TABLE `total_prova`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de tabela `usuarios`
---
-ALTER TABLE `usuarios`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
-COMMIT;
 
