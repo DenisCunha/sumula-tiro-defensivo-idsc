66a4deb809a210a610dfcf0506a4815b7790a19d
deniscunha <ded_2004@hotmail.com>
2024-07-23 11:46:01 -0300

    update function upgrade

--- install/upgrade.php
+++ install/upgrade.php
@@ -5,12 +5,15 @@ error_reporting(E_ALL);
 
 if (file_exists('../system/config.php')) {
 	if (filesize('../system/config.php') > 0) {
-		$install = true;
+		$upgrade = true;
 	} else  {
-    $install = false;
+    $upgrade = false;
   }
-} else {
-  $error['config_file'] = 'Aviso: o arquivo system/config.php não existe, por favor renomear o arquivo config-dist.php para config.php!';
+}
+
+// Configuration
+if (is_file('../system/config.php')) {
+	require_once('../system/config.php');
 }
 
 $error = array();
@@ -38,7 +41,7 @@ if (!array_filter(array('mysql', 'mysqli', 'pdo', 'pgsql'), 'extension_loaded'))
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
-<title>Pista Competição IDSC - Instalação</title>
+<title>Pista Competição IDSC - Atualização</title>
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
@@ -49,8 +52,8 @@ if (!array_filter(array('mysql', 'mysqli', 'pdo', 'pgsql'), 'extension_loaded'))
 </head>
 <body class="bg-light">
 <div class="container">
-<?php if($install) { ?> 
-<h3>Sistema já instalado</h3>
+<?php if($upgrade) { ?> 
+<h3>Sistema Atualizado</h3>
 <script type="text/javascript">
 setTimeout(() => {
  window.location.href = "/";
@@ -69,63 +72,6 @@ setTimeout(() => {
   <?php } ?>
 <?php } else { ?>
 
-<form action="/install/setup.php" method="post" class="form-inline" autocomplete="off" enctype="multipart/form-data">
-<div class="row form-row">
-
-<fieldset>
-<legend>Informações do Banco de dados</legend>
-<br>
-<div class="col">
-<label>DB Drive: </label><input type="text" name="db_driver"  class="form-control" value="mysqli" required>
-</div>
-
-<div class="col">
-<label>Hostname: </label><input type="text" name="db_hostname"  class="form-control" value="localhost" required>
-</div>
-
-<div class="col">
-<label>DB Username: </label><input type="text" name="db_username"  class="form-control" value="" required>
-</div>
-
-<div class="col">
-<label>DB Password: </label><input type="text" name="db_password"  class="form-control" value="" required>
-</div>
-
-<div class="col">
-<label>DB Name: </label><input type="text" name="db_database"  class="form-control" value="" required>
-</div>
-
-<div class="col">
-<label>DB Port: </label><input type="text" name="db_port"  class="form-control" value="3306" required>
-</div>
-
-<div class="col">
-<label>DB Prefix: </label><input type="text" name="db_prefix"  class="form-control" value="">
-</div>
-
-
-</fieldset>
-
-<fieldset>
-<legend>Informações do usuário admin</legend>
-<br>
-<div class="col">
-<label>Usuário: </label><input type="text" name="username"  class="form-control" value="" required>
-</div>
-<div class="col">
-<label>Senha: </label><input type="text" name="password"  class="form-control" value="" required>
-</div>
-
-<br>
-<div class="d-grid gap-2">
-<input class="btn btn-primary btn-lg" type="submit" value="Enviar">
-</div>
-<br>
-</fieldset>    
-</div>
-
-
-</form>
 <?php } ?>
 <?php } ?>
 <br>
